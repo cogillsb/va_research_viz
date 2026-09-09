1d697fe6c3a3c2cb908de55ef7eea5805ac35e04
cogillsb-urt <cogillsb@urtech.io>
2026-09-09 12:17:15 -0400

    limited strong findings to first degree

--- front_end_v3.py
+++ front_end_v3.py
@@ -377,7 +377,8 @@ if st.session_state.studies is not None:
     
     #Add in the graphs
     for b in st.session_state.studies.Bin.unique():
-        df_time = st.session_state.studies[st.session_state.studies.Bin==b].head(10)
+        df_first_studies = st.session_state.studies[(st.session_state.studies.Bin==b) and ((st.session_state.studies.Degree==1))]
+        df_time = df_first_studies.head(10)
         df_time.sort_values(by='Date', inplace=True)
         dates = []
         titles = []
